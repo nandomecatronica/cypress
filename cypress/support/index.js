09ba5a2eac93f15aa12ca25e05bf6c64a5732307
Luiz Arruda <nandomecatronica@gmail.com>
2021-01-13 17:43:16 -0300

    Add relatorio allure

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,5 +16,9 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+import '@shelex/cypress-allure-plugin';
+//with require:
+require('@shelex/cypress-allure-plugin');
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
